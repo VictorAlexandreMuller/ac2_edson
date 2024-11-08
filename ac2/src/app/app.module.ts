2ede7d7721e4efe5adb59ca3f1ec5d629943b2cd
Victor Alexandre Müller <victor.alexandre.mu@gmail.com>
2024-11-08 19:55:49 -0300

    component login, bootstrap e ng boots

--- ac2/src/app/app.module.ts
+++ ac2/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [
     provideClientHydration()
